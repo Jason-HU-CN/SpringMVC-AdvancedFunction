cb57e4a2b5d13f993b67029b10bbcfe3da96fe38
Jason <12212025+jason939392848@user.noreply.gitee.com>
2023-05-14 12:02:25 +0800

    login-test1

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -36,15 +36,25 @@ public class UserController {
         String username = user.getUsername();
         String password = user.getPassword();
         //此处模拟从数据库中获取用户名和密码后进行判断
-        if (username != null && username.equals("jason") && password != null && password.equals("666")){
+        if (username != null && username.equals("jason") && password != null && password.equals("666")) {
             //将用户对象添加到Session
-            session.setAttribute("USER_SESSION",user);
+            session.setAttribute("USER_SESSION", user);
             //用户登录成功，转发到系统首页
             return "main";
         }
         //如果用户名和密码不匹配，转发到登录页面，并进行提醒
-        model.addAttribute("msg","用户名或密码错误，请重新登录！");
+        model.addAttribute("msg", "用户名或密码错误，请重新登录！");
         return "login";
     }
 
+    /*
+     * 用户退出
+     * */
+    @RequestMapping("loginout")
+    public String loginout(HttpSession session){
+        //清楚Session
+        session.invalidate();
+        //退出登录后重定向到登录页面
+        return "redirect:tologin";
+    }
 }
