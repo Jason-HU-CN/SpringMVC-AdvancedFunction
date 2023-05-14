64ab7b56168856f070b8eb8a1c3ed1aa02be130b
Jason <12212025+jason939392848@user.noreply.gitee.com>
2023-05-14 11:20:27 +0800

    interceptor-test

--- src/main/java/com/demo/intercepto/MyInterceptor2.java
+++
@@ -1,4 +0,0 @@
-package com.demo.intercepto;
-
-public class MyInterceptor2 {
-}
---
+++ src/main/java/com/demo/interceptor/MyInterceptor.java
@@ -0,0 +1,28 @@
+package com.demo.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+* 实现了HandlerInterceptor接口的自定义拦截器
+* */
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("MyInterceptor...perHandle");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("MyInterceptor...postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("MyInterceptor...afterCompletion");
+    }
+}
